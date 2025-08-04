1828161000cd491946c7ad5d52600eaebc0f1fe0
Akash Raj <akashraj23042003@gmail.com>
2025-08-05 00:24:27 +0530

    making button in hero section

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -41,7 +41,38 @@ import car_image2 from "./car_image2.png"
 import car_image3 from "./car_image3.png"
 import car_image4 from "./car_image4.png"
 
-export const cityList = ['New York', 'Los Angeles', 'Houston', 'Chicago']
+export const cityList = [
+  'Delhi',
+  'Mumbai',
+  'Bangalore',
+  'Hyderabad',
+  'Chennai',
+  'Kolkata',
+  'Pune',
+  'Ahmedabad',
+  'Jaipur',
+  'Lucknow',
+  'Surat',
+  'Indore',
+  'Bhopal',
+  'Chandigarh',
+  'Nagpur',
+  'Patna',
+  'Vadodara',
+  'Visakhapatnam',
+  'Kanpur',
+  'Ludhiana',
+  'Coimbatore',
+  'Rajkot',
+  'Ranchi',
+  'Thiruvananthapuram',
+  'Guwahati',
+  'Mysore',
+  'Jodhpur',
+  'Noida',
+  'Gurgaon'
+];
+
 
 export const assets = {
     logo,
