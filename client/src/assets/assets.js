6435b9d42373db24cb6a8dc8fed4ce8a5879139b
Akash Raj <akashraj23042003@gmail.com>
2025-08-05 15:02:03 +0530

    added testimonial to homepage

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -155,7 +155,7 @@ export const dummyCarData = [
         "pricePerDay": 300,
         "location": "New York",
         "description": "The BMW X5 is a mid-size luxury SUV produced by BMW. The X5 made its debut in 1999 as the first SUV ever produced by BMW.",
-        "isAvaliable": true,
+        "isAvailable": true,
         "createdAt": "2025-04-16T07:26:56.215Z",
     },
     {
@@ -172,7 +172,7 @@ export const dummyCarData = [
         "pricePerDay": 130,
         "location": "Chicago",
         "description": "The Toyota Corolla is a mid-size luxury sedan produced by Toyota. The Corolla made its debut in 2008 as the first sedan ever produced by Toyota.",
-        "isAvaliable": true,
+        "isAvailable": true,
         "createdAt": "2025-04-16T08:33:57.993Z",
     },
     {
@@ -189,7 +189,7 @@ export const dummyCarData = [
         "pricePerDay": 200,
         "location": "Los Angeles",
         "description": "The Jeep Wrangler is a mid-size luxury SUV produced by Jeep. The Wrangler made its debut in 2003 as the first SUV ever produced by Jeep.",
-        "isAvaliable": true,
+        "isAvailable": true,
         "createdAt": "2025-04-16T08:34:39.592Z",
     },
     {
@@ -206,7 +206,7 @@ export const dummyCarData = [
         "pricePerDay": 209,
         "location": "Houston",
         "description": "This is a mid-size luxury sedan produced by Toyota. The Corolla made its debut in 2008 as the first sedan ever produced by Toyota.",
-        "isAvaliable": true,
+        "isAvailable": true,
         "createdAt": "2025-04-17T06:15:47.318Z",
     }
 ];
@@ -220,7 +220,7 @@ export const dummyMyBookingsData = [
         "pickupDate": "2025-06-13T00:00:00.000Z",
         "returnDate": "2025-06-14T00:00:00.000Z",
         "status": "confirmed",
-        "price": 440,
+        "price": 4400,
         "createdAt": "2025-06-10T12:57:48.244Z",
     },
     {
@@ -231,7 +231,7 @@ export const dummyMyBookingsData = [
         "pickupDate": "2025-06-12T00:00:00.000Z",
         "returnDate": "2025-06-12T00:00:00.000Z",
         "status": "pending",
-        "price": 130,
+        "price": 1300,
         "createdAt": "2025-06-10T12:57:25.613Z",
     },
     {
@@ -242,7 +242,7 @@ export const dummyMyBookingsData = [
         "pickupDate": "2025-06-11T00:00:00.000Z",
         "returnDate": "2025-06-12T00:00:00.000Z",
         "status": "pending",
-        "price": 600,
+        "price": 6000,
         "createdAt": "2025-06-10T09:55:06.379Z",
     },
     {
@@ -253,7 +253,7 @@ export const dummyMyBookingsData = [
         "pickupDate": "2025-06-11T00:00:00.000Z",
         "returnDate": "2025-06-12T00:00:00.000Z",
         "status": "confirmed",
-        "price": 440,
+        "price": 4400,
         "createdAt": "2025-06-10T09:44:25.410Z",
     }
 ]
@@ -267,5 +267,5 @@ export const dummyDashboardData = {
         dummyMyBookingsData[0],
         dummyMyBookingsData[1]
     ],
-    "monthlyRevenue": 840
+    "monthlyRevenue": 8400
 }
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className='h-screen flex flex-col items-center justify-center gap-14 bg-light text-center'>
         <h1 className='text-4xl md:text-5xl font-semibold'>
-            Luxury cars on Rent
+            Popular Cars on Rent
         </h1>
         <form className='flex flex-col md:flex-row items-start md:items-center justify-between p-6 rounded-lg md:rounded-full w-full max-w-80 md:max-w-200 bg-white shadow-[0px_8px_20px_rgba(0,0,0,0.1)]'>
             <div className='flex flex-col md:flex-row items-start md:items-center gap-10 min-md:ml-8'>
@@ -27,12 +27,12 @@ const Hero = () => {
                     <label htmlFor='return-date'>Return Date</label>
                     <input type='date' id='return-date' min={new Date().toISOString().split('T')[0]} className='text-sm text-gray-500'required />
                 </div>
-                <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'>
+                
+            </div>
+        <button className='flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer'>
                     <img src={assets.search_icon} alt='search' className='brightness-300'/>
                     Search
                 </button>
-            </div>
-
         </form>
         <img src={assets.main_car} alt="Car" className='max-h-74'/>
     </div>
