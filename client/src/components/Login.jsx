42651a1070c4d7e93fb2af246cd8b6fa9eda18c4
Akash Raj <akashraj23042003@gmail.com>
2025-08-12 22:11:21 +0530

    created dashboard

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='fixed top-0 bottom-0 left-0 right-0 z-100 flex'>
+
+    </div>
+  )
+}
+
+export default Login
--- client/src/pages/owner/ManageCars.jsx
+++ client/src/pages/owner/ManageCars.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { dummyCarData } from '../../assets/assets'
+import { assets, dummyCarData } from '../../assets/assets'
 import Title from '../../components/owner/Title'
 
 
 const ManageCars = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
 
   const[cars,setCars]=useState([])
   const fetchOwnerCars = async()=>{
@@ -35,8 +37,33 @@ const ManageCars = () => {
           </thead>
           <tbody>
             {cars.map((car, index)=>(
-              <tr>
-                
+              <tr key={index} className='border-t border-borderColor'>
+
+                <td className='p-3 flex items-center gap-3'>
+
+                  <img src={car.image} alt="" className='h-12 w-12 aspect-square rounded-md object-cover'/>
+                  <div className='max-md:hidden'>
+                    <p className='font-medium'>{car.brand} {car.model}</p>
+                    <p className='text-xs text-gray-500'>{car.seating_capacity} - {car.transmission}</p>
+                  </div>
+
+                </td>
+                <td className='p-3 max-md:hidden'>
+                  {car.category}
+                </td>
+                <td className='p-3'>
+                  {currency} {car.pricePerDay}/day
+                </td>
+                <td p-3 className='max-md:hidden'>
+                  <span className={`px-3 py-1 rounded-full text-xs ${car.isAvailable ? 'bg-green-100 text-green-500' : 'bg-red-100 text-red-500'}`}>
+                    {car.isAvailable ? "Available" : "Unavailable"} 
+                  </span>
+                </td>
+                <td className='flex items-center p-3'>
+                  <img src={car.isAvailable ? assets.eye_close_icon : assets.eye_icon} alt="" className='cursor-pointer'/>
+                  <img src={assets.delete_icon} alt="" className='cursor-pointer'/>
+                </td>
+
               </tr>
             ))}
           </tbody>
