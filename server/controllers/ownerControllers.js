a5075cb8fd263ef4a3f702b6d6bb0f50b926918d
Akash Raj <akashraj23042003@gmail.com>
2025-08-17 01:54:02 +0530

    successfully test upload car listing at backend api in postman

--- server/controllers/ownerControllers.js
+++ server/controllers/ownerControllers.js
@@ -1,4 +1,7 @@
+import imagekit from "../configs/imageKit.js";
+import Car from "../models/Car.js";
 import User from "../models/User.js";
+import fs from "fs";
 //API to change role to owner
 export const changeRoleToOwner=async(req,res)=>{
     try {
@@ -17,10 +20,30 @@ export const addCar=async(req,res)=>{
         const{_id}=req.user;
         let car=JSON.parse(req.body.carData);
         const imageFile=req.file;
+        //upload image to ImageKit
+        const fileBuffer=fs.readFileSync(imageFile.path)
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder:'/cars'
+        })
+        // optimized through imagekit url transformation
+        var optimizedImageURL = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width:'1280'},//width resizing
+                {quality:'auto'},//auto compression
+                {format:'webp'}//convert to modern format
+            ]
+        });
+        const image=optimizedImageURL;
+        await Car.create({...car,owner: _id,image})
 
-        
+        res.json({success: true,message: "Car added successfully"})
+
 
     } catch (error) {
-        
+        console.log(error.message);
+        res.json({success:false,message:error.message})
     }
 }
